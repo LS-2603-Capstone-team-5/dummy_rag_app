9ca40a2b6d8e6942def2f338d83672880a473e46
Caleb P <cpickard@Desktop.>
2026-05-06 16:42:44 -0400

    fixed typo in disabled response_cache.sql file

--- lecture_rag_backend/src/models/response_cache.sql
+++ lecture_rag_backend/src/models/response_cache.sql
@@ -3,6 +3,6 @@ CREATE TABLE response_cache (
   query_embedding_vector vector(1536) NOT NULL,
   query_text text NOT NULL,
   response_text text NOT NULL,
-  dialogue_mode boolean NOT NULL DEFAULT false;
+  dialogue_mode boolean NOT NULL DEFAULT false,
   created_at timestamptz DEFAULT NOW()
 );
